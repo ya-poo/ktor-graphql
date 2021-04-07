cae3519d7b4486508983f473f48cea25f7d8d056
Kazufumi Okuda <kazufumi.okuda@justincase-tech.com>
2021-04-08 01:14:50 +0900

    fix style of holding version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,20 +10,18 @@ application {
     mainClassName = "com.yapoo.graphql.ApplicationKt"
 }
 
-object Versions {
-    const val ktor = "1.5.2"
-    const val kotlin = "1.4.30"
-    const val graphql = "11.0.0"
-    const val logback = "1.2.3"
-}
+val ktor = "1.5.2"
+val kotlin = "1.4.30"
+val graphql = "11.0.0"
+val logback = "1.2.3"
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:${Versions.kotlin}"))
-    implementation(kotlin("stdlib", Versions.kotlin))
-    implementation("io.ktor:ktor-server-core:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
-    implementation("io.ktor:ktor-jackson:${Versions.ktor}")
-    implementation("ch.qos.logback:logback-classic:${Versions.logback}")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlin"))
+    implementation(kotlin("stdlib", kotlin))
+    implementation("io.ktor:ktor-server-core:$ktor")
+    implementation("io.ktor:ktor-server-netty:$ktor")
+    implementation("io.ktor:ktor-jackson:$ktor")
+    implementation("ch.qos.logback:logback-classic:$logback")
 
-    implementation("com.graphql-java-kickstart:graphql-java-tools:${Versions.graphql}")
+    implementation("com.graphql-java-kickstart:graphql-java-tools:$graphql")
 }
